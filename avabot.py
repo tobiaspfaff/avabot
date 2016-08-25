bcc1b7897fb5487319bdb7a0fab97a51201598e7
Armin Samii <armin@avametric.com>
2016-08-24 19:34:07 -0700

    FTP upload to your favorite server

--- avabot.py
+++ avabot.py
@@ -1,7 +1,8 @@
-import ftplib
 import os
+import pysftp
 import requests
 import time
+import uuid
 
 from slackclient import SlackClient
 
@@ -9,6 +10,11 @@ from slackclient import SlackClient
 # avabot's ID as an environment variable
 BOT_ID = os.environ.get("BOT_ID")
 BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
+FTP_URL = os.environ.get('FTP_URL')
+FTP_USER = os.environ.get('FTP_USER')
+FTP_PASS = os.environ.get('FTP_PASS')
+FTP_DIR = os.environ.get('FTP_DIR')
+FILE_LIST = os.environ.get('FILE_LIST')
 
 # constants
 AT_BOT = "<@" + BOT_ID + ">"
@@ -33,8 +39,8 @@ class File(Message):
         response = slack_client.api_call("files.info", file=str(self.file_id))
         if not response['ok']:
             return "I don't understand your message."
-        userId = response['file']['user']
-        self.channel = userId
+        user_id = response['file']['user']
+        self.channel = user_id
         size = response['file']['size']
         filetype = response['file']['filetype']
         if size > 1024 * 1000:
@@ -50,20 +56,48 @@ class File(Message):
             return "I could not download the image at " + url
 
         # Where should the file go?
-        outputDir = userId
-        if not os.path.exists(outputDir):
-            os.makedirs(outputDir)
-        outputFilename = os.path.join(outputDir, 'output.' + filetype)
+        output_dir = user_id
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        output_filename = str(uuid.uuid4()) + '.' + filetype
+        output_filepath = os.path.join(output_dir, output_filename)
 
         # Read and save the file
-        with open(outputFilename, 'wb') as handle:
+        with open(output_filepath, 'wb') as handle:
             for block in fileResponse.iter_content(1024):
                 handle.write(block)
 
+        # Upload the file
+        upload_image(output_filepath)
+
         return "Thanks for the tip, friend!"
 
-def upload_image():
-    session = ftplib.FTP('server.address.com','USERNAME','PASSWORD')
+def create_js_list(file_list, output_filepath):
+    with open(output_filepath, 'wb') as myfile:
+        myfile.write('function getImages() {\n')
+        myfile.write('  return [\n')
+        for i in file_list:
+            if i:
+                myfile.write('    "' + i + '",\n')
+        myfile.write('] }')
+
+def upload_image(localFile):
+    with pysftp.Connection(FTP_URL, username=FTP_USER, password=FTP_PASS) as sftp:
+        with sftp.cd(FTP_DIR):
+            # Put the image
+            sftp.put(localFile)
+
+            # Add the image's filename to FILE_LIST
+            if localFile: # will only be false on test cases
+                with open(FILE_LIST, "a") as myfile:
+                    myfile.write(os.path.basename(localFile) + "\n")
+
+            # Add contents of FILE_LIST to a js list
+            with open(FILE_LIST, "r") as myfile:
+                file_list = myfile.read().split('\n')
+            output_filepath = 'imagelist.js'
+            create_js_list(file_list, output_filepath)
+            sftp.put(output_filepath)
 
 def handle_command(message):
     if message.type == 'text':
@@ -100,6 +134,8 @@ def parse_slack_output(slack_rtm_output):
 
 
 if __name__ == "__main__":
+    #upload_image('U0E68ATT4/1293699a-d85a-4e78-9281-24896e67de86.jpg')
+    #exit()
     READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
     if slack_client.rtm_connect():
         print("Ava is listening")
